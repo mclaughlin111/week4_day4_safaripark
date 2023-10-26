86b05d578905bb703696abd506a2492f0959d603
Thomas McLaughlin <tmclaughlin5.1111@gmail.com>
2023-10-26 16:21:36 +0100

    Inserted Values into Tables, Troubleshoot Creation Order

--- safaripark.sql
+++ safaripark.sql
@@ -3,7 +3,18 @@ DROP TABLE IF EXISTS enclosure;
 DROP TABLE IF EXISTS staff;
 DROP TABLE IF EXISTS assignment;
 
+CREATE TABLE enclosure (
+    id SERIAL PRIMARY KEY, --REFERENCES animal(enclosure_id),
+    name VARCHAR(255),
+    capacity INT,
+    closedForMaintenance BOOLEAN
+);
 
+CREATE TABLE staff (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR,
+    employeeNumber INT --REFERENCES animal(id)
+);
 
 CREATE TABLE animal (
     id SERIAL PRIMARY KEY,
@@ -13,23 +24,18 @@ CREATE TABLE animal (
     enclosure_id INT REFERENCES enclosure(id)
 );
 
-CREATE TABLE enclosure (
-    id SERIAL PRIMARY KEY REFERENCES animal(enclosure_id),
-    name VARCHAR(255),
-    capacity INT,
-    closedForMaintenance BOOLEAN
-);
-
-CREATE TABLE staff (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR,
-    employeeNumber INT REFERENCES animal(id)
-);
 
 CREATE TABLE assignment (
     id SERIAL PRIMARY KEY,
-    employeeId INT REFERENCES staff(id),
-    enclosureId INT REFERENCES enclosure(id),
+    employeeId INT, --REFERENCES staff(id),
+    enclosureId INT, --REFERENCES enclosure(id),
     day VARCHAR(255)
 );
+INSERT INTO enclosure (name, capacity, closedForMaintenance) VALUES ('big cat field', 20, 'FALSE');
+INSERT INTO animal (name, type, age, enclosure_id) VALUES ('Tony', 'Tiger', 59, 1);
 
+
+
+INSERT INTO staff (name, employeeNumber) VALUES ('Captain Rik', 12345);
+
+INSERT INTO assignment (employeeId, enclosureId, day) VALUES (1, 1, 'Tuesday');
